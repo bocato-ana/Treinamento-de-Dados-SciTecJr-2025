6967895064d68ad7035c91fb360c0dd903d9fa88
bocato-ana <analuizabocatodasilva@gmail.com>
2025-09-06 19:02:23 -0300

    Update pag1.py

--- pages/pag1.py
+++ pages/pag1.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('Student Mental Health.csv')
+df = pd.read_csv('Student Mental health.csv')
 
 st.title("""Dashboard de Insights
 - Considerações Iniciais
